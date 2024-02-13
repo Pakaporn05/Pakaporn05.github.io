2749d3996f6f45932fb5d2ce8422ab9eaebd9fca
esso <kiatisak.th.64@ubu.ac.th>
2024-02-13 16:24:11 +0700

    เพิ่ม

---
+++ streamlit/app02.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+st.title('OOp in ML')
+st.sidebar.text('sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone')
+
+
+st.image('banner.jpg')
+st.text_input("what's your name?")
+
